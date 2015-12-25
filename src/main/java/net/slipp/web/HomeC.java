c84c4158123e512d3463bcc2da606a658a259115
dw <dongwha10@naver.com>
2015-12-25 19:12:55 +0900

    PJS mvc setting + bootstrap setting END.

---
+++ src/main/java/net/slipp/web/HomeC.java
@@ -0,0 +1,12 @@
+package net.slipp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeC {
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+}
