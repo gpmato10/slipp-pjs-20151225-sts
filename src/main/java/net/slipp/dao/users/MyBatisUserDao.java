2fc75b151df44b1809d5bbd94e4b5c63431251ce
dw <gpmato10@gmail.com>
2016-01-01 23:19:46 +0900

    29-component-scan 설정 추가 04:50 까지 완료.

--- src/main/java/net/slipp/dao/users/MyBatisUserDao.java
+++ src/main/java/net/slipp/dao/users/MyBatisUserDao.java
@@ -1,10 +1,13 @@
 package net.slipp.dao.users;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public class MyBatisUserDao implements UserDao{
 
+	@Autowired
 	private SqlSession sqlSession;
 	
 	public void setSqlSession(SqlSession sqlSession) {
